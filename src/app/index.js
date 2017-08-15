3f2773ce3abe84d8e3dae579befc3c3885e0705c
lhas <luizhrqas@gmail.com>
2017-08-14 21:17:57 -0300

    fixed lint

--- src/app/index.js
+++ src/app/index.js
@@ -45,6 +45,3 @@ io.on('connection', (socket) => {
 
 
 httpServer.listen(port, '0.0.0.0');
-
-console.log('Loucos F.C. Backend is running! :metal:');
-console.log('Loucos F.C. Backend is running! :metal:');
